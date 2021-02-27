7892f40f46d954bd1a2d21dea895023c154f2c55
Arjan <arjanhoek5191@gmail.com>
2021-02-27 19:38:05 +0100

    added some more remarks

--- farm.js
+++ farm.js
@@ -5,7 +5,7 @@ const getYieldForCrop = (crop, ef) => crop.numPlants * getYieldForPlant(crop.pla
 
 const getTotalYield = (crops, ef) => crops.reduce((acc, crop) => acc + getYieldForCrop(crop, ef), 0);
 
-const getCostsForCrop = crop => crop.numPlants * crop.plant.costs;
+const getCostsForCrop = ({ numPlants, plant }) => numPlants * plant.costs;
 
 const getRevenueForCrop = (input, ef) => input.yield ? input.yield * input.plant.salePrice : getYieldForCrop(input, ef) * input.plant.salePrice;
 
@@ -14,3 +14,5 @@ const getProfitForCrop = (input, ef) => getRevenueForCrop(input, ef) - getCostsF
 const getTotalProfit = (crops, ef) => crops.reduce((acc, cur) => acc + getProfitForCrop(cur, ef), 0)
 
 module.exports = { getYieldForPlant, getYieldForCrop, getTotalYield, getCostsForCrop, getRevenueForCrop, getProfitForCrop, getTotalProfit };
+
+// In steps 6-11, two function references are missing: getTotalYield and getRevenueForCrop also need to incorporate the environment factors. I don't what the official solution would say, but in my code the environment factors are only incorporated in the getYieldForPlant function, the other functions are just passing the object further downstream, so technically there would be only one step instead of 6 or even more.
