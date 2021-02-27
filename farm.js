c4d12fe63dcdea89e4bf4fe11eade68b5c171f65
Arjan <arjanhoek5191@gmail.com>
2021-02-27 14:51:06 +0100

    finished tests and functions with no environmental factors

--- farm.js
+++ farm.js
@@ -1,17 +1,19 @@
 
 const getYieldForPlant = plant => plant.yield;
 
-const getYieldForCrop = input => input.numCrops * getYieldForPlant(input.crop);
+const getYieldForCrop = crop => crop.numPlants * getYieldForPlant(crop.plant);
 
 const getTotalYield = input => input.crops.reduce((acc, cur) => acc + getYieldForCrop(cur), 0);
 
-const getCostsForCrop = input => input.numPlants * input.crop.costs;
+const getCostsForCrop = crop => crop.numPlants * crop.plant.costs; // Could also add a getCostsForPlant function
 
 const getRevenueForCrop = input => input.yield
-  ? input.yield * input.crop.salePrice
-  : input.numPlants * input.crop.yield * input.crop.salePrice;
+  ? input.yield * input.plant.salePrice
+  : input.numPlants * input.plant.yield * input.plant.salePrice;
 
 const getProfitForCrop = input => getRevenueForCrop(input) - getCostsForCrop(input);
+
+const getTotalProfit = input => input.crops.reduce((acc, cur) => acc + getProfitForCrop(cur), 0)
 
 module.exports = {
   getYieldForPlant,
@@ -19,5 +21,6 @@ module.exports = {
   getTotalYield,
   getCostsForCrop,
   getRevenueForCrop,
-  getProfitForCrop
+  getProfitForCrop,
+  getTotalProfit
 };
--- farm.test.js
+++ farm.test.js
@@ -4,7 +4,8 @@ const {
   getTotalYield,
   getCostsForCrop,
   getRevenueForCrop,
-  getProfitForCrop
+  getProfitForCrop,
+  getTotalProfit
 } = require("./farm");
 
 describe("getYieldForPlant", () => {
@@ -24,13 +25,15 @@ describe("getYieldForCrop", () => {
       name: "corn",
       yield: 3,
     };
-    const input = {
-      crop: corn,
-      numCrops: 10,
+
+    const crop = {
+      plant: corn,
+      numPlants: 10,
     };
-    expect(getYieldForCrop(input)).toBe(30);
+
+    expect(getYieldForCrop(crop)).toBe(30);
   });
-});
+}); // In the crop object, the crop key was replaced by plant, which didn't make sense, since it refers to the plant instead of a crop (which is the object itself). Moreover, the numCrops is replaced by numPlants, since this value should (according to the function name) refer to a single crop, not to multiple crops. !!!
 
 describe("getTotalYield", () => {
   test("Calculate total yield with multiple crops", () => {
@@ -42,11 +45,18 @@ describe("getTotalYield", () => {
       name: "pumpkin",
       yield: 4,
     };
-    const crops = [
-      { crop: corn, numCrops: 5 },
-      { crop: pumpkin, numCrops: 2 },
-    ];
-    expect(getTotalYield({ crops })).toBe(23);
+
+    const cornCrop = {
+      plant: corn,
+      numPlants: 5
+    };
+    const pumpkinCrop = {
+      plant: pumpkin,
+      numPlants: 2
+    }
+
+    const crops = [cornCrop, pumpkinCrop];
+    expect(getTotalYield({ crops })).toBe(23); // At the beginning of the assignment, it looks kind of weird to pass the crops array inside an object, being its only property. Only at the end of the assignment it becomes clear why this is.
   });
 
   test("Calculate total yield with 0 amount", () => {
@@ -54,7 +64,7 @@ describe("getTotalYield", () => {
       name: "corn",
       yield: 3,
     };
-    const crops = [{ crop: corn, numCrops: 0 }];
+    const crops = [{ plant: corn, numPlants: 0 }];
     expect(getTotalYield({ crops })).toBe(0);
   });
 });
@@ -66,67 +76,105 @@ describe("getCostsForCrop", () => {
       name: "corn",
       costs: 1.50,
     };
-    const input = {
-      crop: corn,
+    const crop = {
+      plant: corn,
       numPlants: 120
     }
-    expect(getCostsForCrop(input)).toBe(180);
+    expect(getCostsForCrop(crop)).toBe(180);
   });
 });
 
 describe("getRevenueForCrop", () => {
-  test("Get revenue for crop, yield per plant", () => {
+  test("Get revenue for crop, yield per plant, with no environment factors", () => {
     const corn = {
       name: "corn",
       salePrice: 3,
       yield: 2
     };
-    const input = {
-      crop: corn,
+    const crop = {
+      plant: corn,
       numPlants: 120,
     }
-    expect(getRevenueForCrop(input)).toBe(720)
+    expect(getRevenueForCrop(crop)).toBe(720)
   })
 
-  test("Get revenue for crop, yield per crop", () => {
+  test("Get revenue for crop, yield per crop, with no environment factors", () => {
     const corn = {
       name: "corn",
       salePrice: 2.50,
     };
-    const input = {
-      crop: corn,
+    const crop = {
+      plant: corn,
       yield: 150
     }
-    expect(getRevenueForCrop(input)).toBe(375)
+    expect(getRevenueForCrop(crop)).toBe(375)
   })
 })
 
 describe("getProfitForCrop", () => {
-  test("Get profit for crop, yield per plant", () => {
+  test("Get profit for crop, yield per plant, with no environment factors", () => {
     const corn = {
       name: "corn",
       salePrice: 5,
       costs: 3,
       yield: 2
     };
-    const input = {
-      crop: corn,
+    const crop = {
+      plant: corn,
       numPlants: 100
     }
-    expect(getProfitForCrop(input)).toBe(700)
+    expect(getProfitForCrop(crop)).toBe(700)
   })
 
-  test("Get profit for crop, yield per crop", () => {
+  test("Get profit for crop, yield per crop, with no environment factors", () => {
     const corn = {
       name: "corn",
       salePrice: 4,
       costs: 2,
     };
-    const input = {
-      crop: corn,
+    const crop = {
+      plant: corn,
       numPlants: 100,
       yield: 150
     }
-    expect(getProfitForCrop(input)).toBe(400)
+    expect(getProfitForCrop(crop)).toBe(400)
   })
 })
+
+describe("getTotalProfit", () => {
+  test("Calculate total yield with multiple crops, with no environment factors", () => {
+    const corn = {
+      name: "corn",
+      salePrice: 4,
+      costs: 2,
+      yield: 1
+    };
+    const cornCrop = {
+      plant: corn,
+      numPlants: 100
+    }
+    const pumpkin = {
+      name: "corn",
+      salePrice: 7,
+      costs: 4,
+      yield: 2
+    };
+    const pumpkinCrop = {
+      plant: pumpkin,
+      numPlants: 50
+    }
+    const crops = [cornCrop, pumpkinCrop];
+    expect(getTotalProfit({ crops })).toBe(700);
+  });
+
+  test("Calculate total profit with 0 amount", () => {
+    const corn = {
+      name: "corn",
+      salePrice: 5,
+      costs: 3,
+      yield: 2
+    };
+    const crops = [{ plant: corn, numPlants: 0 }];
+    expect(getTotalProfit({ crops })).toBe(0);
+  });
+});
